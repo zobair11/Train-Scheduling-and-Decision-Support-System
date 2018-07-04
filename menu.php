87127bdd1bdf59f48a804d2baa46c782cd1546d9
zobair11 <zobair619316@gmail.com>
2018-07-04 14:54:46 +0700

    Menu

---
+++ menu.php
@@ -0,0 +1,21 @@
+ <div class="menu">
+									<ul id="menu" >
+										<li><a href="index.php"><i class="fa fa-tachometer"></i> <span>Home</span></a></li>
+										 <li id="menu-academico" ><a href="train_detail.php"><i class="fa fa-table"></i> <span> Train Information</span> </a>
+										
+										</li>
+										
+										
+										 <li id="menu-academico" ><a href="station_detail.php"><i class="fa fa-table"></i> <span> Station Information</span> </a>
+										
+										</li>
+										
+										 <li id="menu-academico" ><a href="tnx.php"><i class="fa fa-file-text-o"></i> <span>Train Transaction</span></a></li>
+									<li><a href="crossing_detection.php"><i class="lnr lnr-pencil"></i> <span>Calculation Simulation 1 : <br> Crossing Point Detection  </span></a></li>
+									<li><a href="crossing_detection2.php"><i class="lnr lnr-pencil"></i> <span>Calculation Simulation 2 : <br> Crossing Point Detection  </span></a></li>
+									<li><a href="crossing_detection3.php"><i class="lnr lnr-pencil"></i> <span>Calculation Simulation 3 : <br> Crossing Point Detection  </span></a></li>
+									
+									
+									 
+								  </ul>
+								</div>
