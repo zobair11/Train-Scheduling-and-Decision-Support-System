22cbb75e62a43166fddcbe60c5e5f867e5590261
zobair11 <zobair619316@gmail.com>
2018-07-04 14:53:49 +0700

    Login for getting real time data

---
+++ get.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html><center>
+<body  bgcolor="red">
+<form action="getpro.php" method="POST">
+Username : <input style="
+    background: #FFF;
+    border-radius: 10px;
+    border: 1px solid #FFF;
+    line-height: 40px;
+    height: 40px;
+    padding: 0 8px;
+    width: 300px;
+    font-size: 12pt;
+    font-weight: 400;
+    font-family: 'Open Sans', Arial, sans-serif;
+" type="text" name="u"> <br><br>
+Password : <input style="
+    background: #FFF;
+    border-radius: 10px;
+    border: 1px solid #FFF;
+    line-height: 40px;
+    height: 40px;
+    padding: 0 8px;
+    width: 300px;
+    font-size: 12pt;
+    font-weight: 400;
+    font-family: 'Open Sans', Arial, sans-serif;
+" type="password" name="p"><br> <br>
+<input style="
+   
+    border-radius: 10px;
+    border: 1px solid #FFF;
+    line-height: 40px;
+    height: 40px;
+    padding: 0 8px;
+    width: 300px;
+    font-size: 12pt;
+    font-weight: 400;
+    font-family: 'Open Sans', Arial, sans-serif;
+" type="submit" name="s">
+
+
+
+</form>
+
+</body>
+</html>
+</center>
